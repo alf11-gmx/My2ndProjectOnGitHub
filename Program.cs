1f79ef74f8f5a9045b26490e4de531cd74cce375
Alf11 GMX <alf11.spalt@gmx.at>
2025-09-04 18:26:54 +0200

    Versuch #2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,1 @@
-﻿Console.WriteLine("A");
-Console.WriteLine("alf11-first-branch");
-Console.WriteLine("spaa-first-branch");
-Console.WriteLine("spaa-second-commit");
+﻿Console.WriteLine("main - start");
